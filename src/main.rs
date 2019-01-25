44b2a47ead7706187e57d37d3af805d89489aa46
CoolOppo <CoolOppo@users.noreply.github.com>
2019-01-25 17:09:20 -0500

    linux support + prompt user if no args provided

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ use hashbrown::HashMap;
 use hound::{SampleFormat, WavReader, WavSpec};
 use std::env;
 use std::error::Error;
+use std::io;
 use std::io::BufReader;
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let words = &args[1..];
+    let mut words: Vec<String> = args[1..].to_vec();
+    if words.iter().count() < 1 {
+        let mut read_words = String::new();
+        println!("Enter a string: ");
+        io::stdin().read_line(&mut read_words)?;
+        words = read_words
+            .split_whitespace()
+            .map(|s| String::from(s))
+            .collect();
+    }
+
     let dict: HashMap<String, Vec<String>> =
         deserialize(include_bytes!("../build/ser.bin")).unwrap();
     let wav_files = {
@@ -46,7 +57,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             .collect();
         let symbols = dict
             .get(&word)
-            .unwrap_or_else(|| panic!("Could not find {} in dictionary.", word));
+            .unwrap_or_else(|| panic!("Could not find \"{}\" in dictionary.", word));
 
         for symbol in symbols.iter() {
             for sample in match symbol.as_str() {
@@ -96,10 +107,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
     output_wav.finalize()?;
-    Command::new("cmd")
-        .args(&["/C", "ffplay output.wav"])
-        .output()
-        .unwrap_or_else(|_| panic!("Failed to execute ffplay"));
+
+    if cfg!(target_os = "windows") {
+        let out = Command::new("cmd")
+            .args(&["/C", "start output.wav"])
+            .output()
+            .unwrap_or_else(|_| panic!("Failed to open output.wav"));
+        if !out.stderr.is_empty() {
+            println!("{}", std::str::from_utf8(&out.stderr).unwrap());
+        }
+    } else {
+        let out = Command::new("sh")
+            .arg("-c")
+            .arg("xdg-open output.wav")
+            .output()
+            .unwrap_or_else(|_| panic!("Failed to open output.wav"));
+        if !out.stderr.is_empty() {
+            println!("{}", std::str::from_utf8(&out.stderr).unwrap());
+        }
+    }
 
     Ok(())
 }
